11e27aa576c914972425654c6f6e02a0e8f1a350
Thomas Dekiere <t.dekiere@gmail.com>
2021-10-12 12:49:23 +0200

    fix: always show non numeric scores on top of scoreboard

--- src/modules/cards/cards.ts
+++ src/modules/cards/cards.ts
@@ -7,9 +7,11 @@ export const getCards = (): PlayCard[] => [
 export const sortCards = (a: PlayCard, b: PlayCard) => {
   if (a === b) return 0;
 
-  if (a === 'coffee') return 1;
+  if (a === 'coffee') return -1;
+  if (b === 'coffee') return 1;
 
   if (a === '?') return -1;
+  if (b === '?') return 1;
 
   return a < b ? 1 : -1;
 };
